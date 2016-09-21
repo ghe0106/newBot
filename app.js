7e21997bfb64ff2046781f6f2a343fcd7a880c11
admin <admin@admins-MacBook-Pro.local>
2016-09-21 09:42:49 +0100

    changes to app.js

--- app.js
+++ app.js
@@ -1,19 +1,59 @@
-//requirements
-let restify = require('restify'),
-    builder = require('botbuilder');
+var builder = require('botbuilder');
+var restify = require('restify');
 
-//set up restify server
-let server = restify.createServer();
-server.listen(process.env.PORT || 3000, function() {
-    console.log('%s listening to %s', server.name, server.url);
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url);
 });
 
-//create chat bot
-let connector = new builder.ChatConnector({appId: 'a986a103-1366-44cc-8f92-7e2d066081b0', appPassword: 'YDQ9cLT6omhjQK4pRQDizEd'});
-let bot = new builder.UniversalBot(connector);
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: 'a986a103-1366-44cc-8f92-7e2d066081b0',
+    appPassword: 'YDQ9cLT6omhjQK4pRQDizEd'
+});
+var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-//create bot dialogs
-bot.dialog('/', function(session) {
-    session.send("Hello World!");
-});
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+var intents = new builder.IntentDialog();
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
