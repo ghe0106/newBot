6bd0b296121390a6a795c8d5cc6090d70761e765
admin <george.easton@prudential.co.uk>
2016-09-22 12:54:04 +0100

    updated to waterfall app

--- app.js
+++ app.js
@@ -1,141 +1,94 @@
 // Requirements
 "use strict";
 
-/*
-  REST-enabled version of LUIS alarm demo
-*/
+var builder = require('botbuilder');
+var restify = require('restify');
 
-const restify = require('restify');
-const builder = require('botbuilder');
+//=========================================================
+// Bot Setup
+//=========================================================
 
-
-// Set up restify server
-const server = restify.createServer();
-server.listen(process.env.PORT || 3000, function() {
-  console.log('%s listening to %s', server.name, server.url);
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3000, function () {
+   console.log('%s listening to %s', server.name, server.url);
 });
 
-const connector = new builder.ChatConnector({
+// Create chat bot
+var connector = new builder.ChatConnector({
   appId: process.env.MSFT_APP_ID,
   appPassword: process.env.MSFT_APP_SECRET
 });
-
-const bot = new builder.UniversalBot(connector);
-
-// Bot chat messages
+var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 // Web chat UI request
 server.get('/', restify.serveStatic({
- directory: __dirname,
- default: '/index.html'
+  directory: __dirname,
+  default: '/index.html'
 }));
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+
 
 // Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
 var model = process.env.model || 'https://api.projectoxford.ai/luis/v1/application?id=c413b2ef-382c-45bd-8ff0-f76d60e2a821&subscription-key=6d0966209c6e4f6b835ce34492f3e6d9&q=';
 var recognizer = new builder.LuisRecognizer(model);
-var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
-bot.dialog('/', dialog);
+var intents = new builder.IntentDialog({ recognizers: [recognizer] });
+bot.dialog('/', intents);
 
-// Add intent handlers
-dialog.matches('builtin.intent.alarm.set_alarm', [
+
+intents.matches(/^start again/i, [
+    function (session) {
+        session.beginDialog('/profile');
+    }
+]);
+
+intents.onDefault([
     function (session, args, next) {
-        // Resolve and store any entities passed from LUIS.
-        var title = builder.EntityRecognizer.findEntity(args.entities, 'builtin.alarm.title');
-        var time = builder.EntityRecognizer.resolveTime(args.entities);
-        var alarm = session.dialogData.alarm = {
-          title: title ? title.entity : null,
-          timestamp: time ? time.getTime() : null
-        };
-
-        // Prompt for title
-        if (!alarm.title) {
-            builder.Prompts.text(session, 'What would you like to call your alarm?');
+        if (!session.userData.age) {
+            session.beginDialog('/profile');
         } else {
             next();
         }
+    },
+    function (session, results) {
+        session.send('Based on your answers, you could get £12345 per year.');
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! I can help you explore your options at retirement. To get started, can you tell me how old you are?');
     },
     function (session, results, next) {
-        var alarm = session.dialogData.alarm;
-        if (results.response) {
-            alarm.title = results.response;
-        }
-
-        // Prompt for time (title will be blank if the user said cancel)
-        if (alarm.title && !alarm.timestamp) {
-            builder.Prompts.time(session, 'What time would you like to set the alarm for?');
-        } else {
-            next();
-        }
+        session.userData.age = results.response;
+        next();
     },
-    function (session, results) {
-        var alarm = session.dialogData.alarm;
-        if (results.response) {
-            var time = builder.EntityRecognizer.resolveTime([results.response]);
-            alarm.timestamp = time ? time.getTime() : null;
-        }
-
-        // Set the alarm (if title or timestamp is blank the user said cancel)
-        if (alarm.title && alarm.timestamp) {
-            // Save address of who to notify and write to scheduler.
-            alarm.address = session.message.address;
-            alarms[alarm.title] = alarm;
-
-            // Send confirmation to user
-            var date = new Date(alarm.timestamp);
-            var isAM = date.getHours() < 12;
-            session.send('Creating alarm named "%s" for %d/%d/%d %d:%02d%s',
-                alarm.title,
-                date.getMonth() + 1, date.getDate(), date.getFullYear(),
-                isAM ? date.getHours() : date.getHours() - 12, date.getMinutes(), isAM ? 'am' : 'pm');
-        } else {
-            session.send('Ok... no problem.');
-        }
+    function (session, results, next) {
+      builder.Prompts.text(session, 'Next, could you tell me how much your pension pot is worth, in pounds?');
+    },
+    function (session, results, next) {
+      session.userData.pensionPot = results.response;
+      next();
+    },
+    function (session, results, next) {
+      builder.Prompts.text(session, 'How much of this would you like to take out straight away, in pounds?');
+    },
+    function (session, results, next) {
+      session.userData.lumpSum = results.response;
+      next();
+    },
+    function (session, results, next) {
+      builder.Prompts.choice(session, 'Would you like to use the remaining money to buy a guaranteed income for life, or would you prefer more control over the amount of income and how long it lasts?',
+        ['guaranteed', 'flexible']);
+    },
+    function (session, results, next) {
+      session.userData.choice = results.response.entity;
+      session.send("So you're %s and have £%s in your fund, of which you'd like to take £%s straight away, and use the remaining amount to buy a %s income.",
+        session.userData.age, session.userData.pensionPot, session.userData.lumpSum, session.userData.choice);
     }
 ]);
-
-dialog.matches('builtin.intent.alarm.delete_alarm', [
-    function (session, args, next) {
-        // Resolve entities passed from LUIS.
-        var title;
-        var entity = builder.EntityRecognizer.findEntity(args.entities, 'builtin.alarm.title');
-        if (entity) {
-            // Verify its in our set of alarms.
-            title = builder.EntityRecognizer.findBestMatch(alarms, entity.entity);
-        }
-
-        // Prompt for alarm name
-        if (!title) {
-            builder.Prompts.choice(session, 'Which alarm would you like to delete?', alarms);
-        } else {
-            next({ response: title });
-        }
-    },
-    function (session, results) {
-        // If response is null the user canceled the task
-        if (results.response) {
-            delete alarms[results.response.entity];
-            session.send("Deleted the '%s' alarm.", results.response.entity);
-        } else {
-            session.send('Ok... no problem.');
-        }
-    }
-]);
-
-dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I can only create & delete alarms."));
-
-// Very simple alarm scheduler
-var alarms = {};
-setInterval(function () {
-    var now = new Date().getTime();
-    for (var key in alarms) {
-        var alarm = alarms[key];
-        if (now >= alarm.timestamp) {
-            var msg = new builder.Message()
-                .address(alarm.address)
-                .text("Here's your '%s' alarm.", alarm.title);
-            bot.send(msg);
-            delete alarms[key];
-        }
-    }
-}, 15000);
